7e5709d6771ea3a3e3e1337ad57d520c953702a5
Kate Tester <katemorris@gmail.com>
2021-01-08 10:36:21 -0700

    Edits to restrict to 10 items

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,9 +1,9 @@
 class SearchFacade
   def self.get_poems(author_search)
     poems = SearchService.poems_by_author(author_search)
-    poems.map do |poem|
-      tone = ToneService.tone_analyzer(poem[:lines].join(", "))
-      Poem.new(poem, tone)
+    poems[0..9].map do |poem|
+      tones = ToneService.tone_analyzer(poem[:lines].join(", "))
+      Poem.new(poem, tones)
     end
   end
 end
